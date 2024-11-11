b2203a9d9d4872582a9e6373f5e9149d19cc143b
sxcntqnt <getinada@gmail.com>
2024-11-11 18:07:05 +0300

    Directory structure change

--- android/app/src/main/kotlin/com/example/bus_reservation_udemy/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bus_reservation_udemy
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/utils/colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-const seatBookedColor = Colors.red;
-final seatAvailableColor = Colors.grey.shade300;
-final seatSelectedColor = Colors.green.shade400;
